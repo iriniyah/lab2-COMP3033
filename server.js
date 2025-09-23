1e3b7c4b163834fd53466af2ad2a8f63f81de3fc
Shania <113151292+iriniyah@users.noreply.github.com>
2025-09-23 19:59:31 -0400

    Added JSON response obj

--- server.js
+++ server.js
@@ -7,8 +7,19 @@ const app = connect();
 
 // Middlewear function for calculator 
 function calculator(req, res, next) {
+    // Set the content type as JSON
     res.setHeader("Content-Type", "application/json");
-    res.end("Hello world of APIs");
+
+    // Response Object for the equation with placeholder data
+    let equationResObj = {
+        x: 1,
+        y: 2,
+        operation: 3,
+        result: 24
+    };
+
+    // Converts the JSON Obj to a JSON formatted string to pass through the argument 
+    res.end(JSON.stringify(equationResObj));
 }
 
 // Associate function to path
